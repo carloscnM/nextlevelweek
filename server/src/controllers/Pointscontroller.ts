63167da8b178d45707cf4f3a2cce6b2e7605388d
Carlos Mario <oliveiracarlosmario1@gmail.com>
2020-06-05 20:15:46 -0300

    Advanced features - Upload and validations

--- server/src/controllers/Pointscontroller.ts
+++ server/src/controllers/Pointscontroller.ts
@@ -19,7 +19,15 @@ class PointsController{
             .distinct()
             .select('points.*');
 
-        res.json(points);
+
+        const serializedPoitns = points.map(point => {
+            return {
+                ...point,
+                image : `http://192.168.0.148:3333/uploads/${point.image}`
+            };
+        });
+    
+        return res.json(serializedPoitns);
     }
 
     async show(req : Request, res:Response) {
@@ -31,13 +39,18 @@ class PointsController{
             return res.status(400).json({message: 'Point not found'});
         }
 
+        const serializedPoitn = {
+                ...point,
+                image : `http://192.168.0.148:3333/uploads/${point.image}`
+        };
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=' , 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
 
-        return res.json({point, items});
+        return res.json({'point':serializedPoitn, items});
     }
 
     async create(req : Request, res:Response){
@@ -58,7 +71,7 @@ class PointsController{
         const trx  = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -73,11 +86,14 @@ class PointsController{
         const point_id = insertedIds[0];
 
 
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id
-            };
+        const pointItems = items
+            .split(',')
+            .map((item:string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return { 
+                    item_id,
+                    point_id
+                };
         });
 
         await trx('point_items').insert(pointItems);
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -4,6 +4,8 @@ import { FiArrowLeft} from 'react-icons/fi';
 import {Map, TileLayer, Marker} from 'react-leaflet';
 import {LeafletMouseEvent} from 'leaflet';
 
+import DropZone from '../../components/Dropzone';
+
 import axios from 'axios';
 import api from '../../services/api';
 
@@ -33,6 +35,7 @@ const CreatePoints = () =>{
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [seletedFile, setSelectedFile] = useState<File>()
 
     const [formData, setFormData] = useState({
         name: '',
@@ -122,16 +125,23 @@ const CreatePoints = () =>{
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf, 
-            city,
-            latitude, 
-            longitude,
-            items
+        const data = new FormData()
+
+        
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if(seletedFile){
+            data.append('image', seletedFile);
         }
+
+
 
         await api.post('points', data);
         
@@ -151,6 +161,9 @@ const CreatePoints = () =>{
             </header>
             <form onSubmit={handleSubmit}>
                  <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <DropZone onFileUpload={setSelectedFile} />
+
 
                  <fieldset>
                      <legend>
